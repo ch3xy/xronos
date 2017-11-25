e24af34b52282a1ee8c8e1ebfca0889c7e2b4647
igor.strilic <igor.strilic@xebris.com>
2017-11-25 15:23:28 +0100

    simple spring security concept + test api

--- src/main/java/com/ch3xy/xronos/XronosApplication.java
+++ src/main/java/com/ch3xy/xronos/XronosApplication.java
@@ -2,16 +2,14 @@ package com.ch3xy.xronos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XronosApplication extends SpringBootServletInitializer {
+public class XronosApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(XronosApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(XronosApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(XronosApplication.class, args);
